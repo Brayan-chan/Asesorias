3aa6608e56c89a4acd2a3b4c8dcd90bdd85251c4
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-12-12 19:20:34 -0600

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+    public Dog(String name, String colour) {
+        super(name, colour);
+    }
+    
+    public void bark() {
+        System.out.println("I'm barking");
+    }
+    
+}
