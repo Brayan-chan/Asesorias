9c63b908d03f6ff872546973787bbd0460eaba71
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-12-12 19:21:05 -0600

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+
+    public Cat(String name, String colour) {
+        super(name, colour);
+    }
+    
+    void meww() {
+        System.out.println("I'm mewwing");
+    }
+    
+}
